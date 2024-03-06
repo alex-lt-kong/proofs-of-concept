045ce7c25a649f38755ddb76c5df687bdc758791
Alex Kong <alex.lt.kong@connect.usst.hk>
2024-03-06 23:56:10 +0800

    update

--- cpp/07_face-detection/main.cpp
+++ cpp/07_face-detection/main.cpp
@@ -1,166 +1,177 @@
-#include <opencv2/dnn.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/objdetect.hpp>
+// https://docs.opencv.org/4.x/d0/dd4/tutorial_dnn_face.html
 
 #include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+
+#include <opencv2/dnn.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
 using namespace cv;
 using namespace std;
 
-static
-void visualize(Mat& input, int frame, Mat& faces, double fps, int thickness = 2)
-{
-    std::string fpsString = cv::format("FPS : %.2f", (float)fps);
-    if (frame >= 0)
-        cout << "Frame " << frame << ", ";
-    cout << "FPS: " << fpsString << endl;
-    for (int i = 0; i < faces.rows; i++)
-    {
-        // Print results
-        cout << "Face " << i
-             << ", top-left coordinates: (" << faces.at<float>(i, 0) << ", " << faces.at<float>(i, 1) << "), "
-             << "box width: " << faces.at<float>(i, 2)  << ", box height: " << faces.at<float>(i, 3) << ", "
-             << "score: " << cv::format("%.2f", faces.at<float>(i, 14))
-             << endl;
+volatile sig_atomic_t e_flag = 0;
 
-        // Draw bounding box
-        rectangle(input, Rect2i(int(faces.at<float>(i, 0)), int(faces.at<float>(i, 1)), int(faces.at<float>(i, 2)), int(faces.at<float>(i, 3))), Scalar(0, 255, 0), thickness);
-        // Draw landmarks
-        circle(input, Point2i(int(faces.at<float>(i, 4)), int(faces.at<float>(i, 5))), 2, Scalar(255, 0, 0), thickness);
-        circle(input, Point2i(int(faces.at<float>(i, 6)), int(faces.at<float>(i, 7))), 2, Scalar(0, 0, 255), thickness);
-        circle(input, Point2i(int(faces.at<float>(i, 8)), int(faces.at<float>(i, 9))), 2, Scalar(0, 255, 0), thickness);
-        circle(input, Point2i(int(faces.at<float>(i, 10)), int(faces.at<float>(i, 11))), 2, Scalar(255, 0, 255), thickness);
-        circle(input, Point2i(int(faces.at<float>(i, 12)), int(faces.at<float>(i, 13))), 2, Scalar(0, 255, 255), thickness);
-    }
-    putText(input, fpsString, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 2);
+static void signal_handler(int signum) {
+  char msg[] = "Signal [  ] caught\n";
+  msg[8] = '0' + (char)(signum / 10);
+  msg[9] = '0' + (char)(signum % 10);
+  write(STDIN_FILENO, msg, strlen(msg));
+  e_flag = 1;
 }
 
-int main(int argc, char** argv)
-{
-    cout << cv::getBuildInformation() << "\n";
-    CommandLineParser parser(argc, argv,
-        "{help  h           |            | Print this message}"
-        "{image1 i1         |            | Path to the input image1. Omit for detecting through VideoCapture}"
-        "{image2 i2         |            | Path to the input image2. When image1 and image2 parameters given then the program try to find a face on both images and runs face recognition algorithm}"
-        "{video v           | 0          | Path to the input video}"
-        "{scale sc          | 1.0        | Scale factor used to resize input video frames}"
-        "{fd_model fd       | face_detection_yunet_2021dec.onnx| Path to the model. Download yunet.onnx in https://github.com/opencv/opencv_zoo/tree/master/models/face_detection_yunet}"
-        "{fr_model fr       | face_recognition_sface_2021dec.onnx | Path to the face recognition model. Download the model at https://github.com/opencv/opencv_zoo/tree/master/models/face_recognition_sface}"
-        "{score_threshold   | 0.9        | Filter out faces of score < score_threshold}"
-        "{nms_threshold     | 0.3        | Suppress bounding boxes of iou >= nms_threshold}"
-        "{top_k             | 5000       | Keep top_k bounding boxes before NMS}"
-        "{save s            | false      | Set true to save results. This flag is invalid when using camera}"
-    );
-    if (parser.has("help"))
-    {
-        parser.printMessage();
-        return 0;
+void install_signal_handler() {
+  // This design canNOT handle more than 99 signal types
+  if (_NSIG > 99) {
+    fprintf(stderr, "signal_handler() can't handle more than 99 signals\n");
+    abort();
+  }
+  struct sigaction act;
+  // Initialize the signal set to empty, similar to memset(0)
+  if (sigemptyset(&act.sa_mask) == -1) {
+    perror("sigemptyset()");
+    abort();
+  }
+  act.sa_handler = signal_handler;
+  /* SA_RESETHAND means we want our signal_handler() to intercept the signal
+  once. If a signal is sent twice, the default signal handler will be used
+  again. `man sigaction` describes more possible sa_flags. */
+  act.sa_flags = SA_RESETHAND;
+  // act.sa_flags = 0;
+  if (sigaction(SIGINT, &act, 0) == -1) {
+    perror("sigaction()");
+    abort();
+  }
+}
+
+static void visualize(Mat &input, int frame, Mat &faces, double fps,
+                      int thickness = 2) {
+  std::string fpsString = cv::format("FPS : %.2f", (float)fps);
+  if (frame >= 0)
+    cout << "Frame " << frame << ", ";
+  cout << "FPS: " << fpsString << endl;
+  for (int i = 0; i < faces.rows; i++) {
+    // Print results
+    cout << "Face " << i << ", top-left coordinates: (" << faces.at<float>(i, 0)
+         << ", " << faces.at<float>(i, 1) << "), "
+         << "box width: " << faces.at<float>(i, 2)
+         << ", box height: " << faces.at<float>(i, 3) << ", "
+         << "score: " << cv::format("%.2f", faces.at<float>(i, 14)) << endl;
+
+    // Draw bounding box
+    rectangle(input,
+              Rect2i(int(faces.at<float>(i, 0)), int(faces.at<float>(i, 1)),
+                     int(faces.at<float>(i, 2)), int(faces.at<float>(i, 3))),
+              Scalar(0, 255, 0), thickness);
+    // Draw landmarks
+    circle(input,
+           Point2i(int(faces.at<float>(i, 4)), int(faces.at<float>(i, 5))), 2,
+           Scalar(255, 0, 0), thickness);
+    circle(input,
+           Point2i(int(faces.at<float>(i, 6)), int(faces.at<float>(i, 7))), 2,
+           Scalar(0, 0, 255), thickness);
+    circle(input,
+           Point2i(int(faces.at<float>(i, 8)), int(faces.at<float>(i, 9))), 2,
+           Scalar(0, 255, 0), thickness);
+    circle(input,
+           Point2i(int(faces.at<float>(i, 10)), int(faces.at<float>(i, 11))), 2,
+           Scalar(255, 0, 255), thickness);
+    circle(input,
+           Point2i(int(faces.at<float>(i, 12)), int(faces.at<float>(i, 13))), 2,
+           Scalar(0, 255, 255), thickness);
+  }
+  putText(input, fpsString, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5,
+          Scalar(0, 255, 0), 2);
+}
+
+int main(int argc, char **argv) {
+  install_signal_handler();
+  if (argc < 2 || argc > 3) {
+    cerr << "Usage: " << argv[0] << " <inputVideoPath> [outputVideoPath]"
+         << endl;
+    return EXIT_FAILURE;
+  }
+  cout << "OpenCV's build information:\n" << cv::getBuildInformation() << "\n";
+
+  /// Model parameters
+  // the threshold to filter out bounding boxes of score smaller than the given
+  // value
+  float scoreThreshold = 0.9;
+  // the threshold to suppress bounding boxes of IoU bigger than the given value
+  float nmsThreshold = 0.3;
+  // keep top K bboxes before NMS
+  int topK = 5000;
+  /// Model parameters
+
+  // Initialize FaceDetectorYN
+  Ptr<FaceDetectorYN> detector = FaceDetectorYN::create(
+      "./face_detection_yunet_2023mar.onnx", "", Size(1920, 1080),
+      scoreThreshold, nmsThreshold, topK);
+
+  TickMeter tm;
+
+  int frameWidth, frameHeight;
+  VideoCapture capture;
+  string inputVideo = argv[1];
+  string outputVideo = "";
+  if (argc == 3) {
+    outputVideo = argv[2];
+  }
+  capture.open(samples::findFileOrKeep(inputVideo)); // keep GStreamer pipelines
+  if (capture.isOpened()) {
+    frameWidth = int(capture.get(CAP_PROP_FRAME_WIDTH));
+    frameHeight = int(capture.get(CAP_PROP_FRAME_HEIGHT));
+    cout << "Video " << inputVideo << ": width=" << frameWidth
+         << ", height=" << frameHeight << endl;
+  } else {
+    cout << "Could not initialize video capturing: " << inputVideo << "\n";
+    return 1;
+  }
+
+  detector->setInputSize(Size(frameWidth, frameHeight));
+  VideoWriter writer;
+  if (outputVideo.length() > 0) {
+    writer = VideoWriter(outputVideo, VideoWriter::fourcc('M', 'P', '4', 'V'),
+                         30, Size(frameWidth, frameHeight));
+  }
+
+  int nFrame = 0;
+  while (!e_flag) {
+    // Get frame
+    Mat frame;
+    if (!capture.read(frame)) {
+      cerr << "Can't grab frame! Stop\n";
+      break;
     }
+    resize(frame, frame, Size(frameWidth, frameHeight));
 
-    String fd_modelPath = parser.get<String>("fd_model");
-    cout << "fd_modelPath: " << fd_modelPath << endl;
+    // Inference
+    Mat faces;
+    tm.start();
+    detector->detect(frame, faces);
+    tm.stop();
 
-    float scoreThreshold = parser.get<float>("score_threshold");
-    float nmsThreshold = parser.get<float>("nms_threshold");
-    int topK = parser.get<int>("top_k");
+    Mat result = frame.clone();
+    // Draw results on the input image
+    visualize(result, nFrame, faces, tm.getFPS());
 
-    bool save = parser.get<bool>("save");
-    float scale = parser.get<float>("scale");
+    // Visualize results
+    imshow("Live", result);
 
+    int key = waitKey(1);
 
-    //! [initialize_FaceDetectorYN]
-    // Initialize FaceDetectorYN
-    // 
-    // Ptr<FaceDetectorYN> detector = FaceDetectorYN::create(fd_modelPath, "", Size(320, 320), scoreThreshold, nmsThreshold, topK);
-    Ptr<FaceDetectorYN> detector = FaceDetectorYN::create(
-        "./face_detection_yunet_2023mar.onnx", 
-        "", Size(1920, 1080), scoreThreshold, nmsThreshold, topK);
-    //! [initialize_FaceDetectorYN]
+    if (outputVideo.length() > 0)
+      writer.write(result);
 
-    TickMeter tm;
+    ++nFrame;
+  }
 
-  
-    int frameWidth, frameHeight;
-    VideoCapture capture;
-    std::string video = parser.get<string>("video");
-    video = "/tmp/20240303-205918.mp4";
-    if (video.size() == 1 && isdigit(video[0]))
-        capture.open(parser.get<int>("video"));
-    else
-        capture.open(samples::findFileOrKeep(video));  // keep GStreamer pipelines
-    if (capture.isOpened())
-    {
-        frameWidth = int(capture.get(CAP_PROP_FRAME_WIDTH) * scale);
-        frameHeight = int(capture.get(CAP_PROP_FRAME_HEIGHT) * scale);
-        cout << "Video " << video
-            << ": width=" << frameWidth
-            << ", height=" << frameHeight
-            << endl;
-    }
-    else
-    {
-        cout << "Could not initialize video capturing: " << video << "\n";
-        return 1;
-    }
-
-    detector->setInputSize(Size(frameWidth, frameHeight));
-    cv::VideoWriter writer(
-        "output.mp4", cv::VideoWriter::fourcc('M', 'P', '4', 'V'), 30, cv::Size(frameWidth, frameHeight)
-    );
-    cout << "Press 'SPACE' to save frame, any other key to exit..." << endl;
-    int nFrame = 0;
-    for (;;)
-    {
-        // Get frame
-        Mat frame;
-        if (!capture.read(frame))
-        {
-            cerr << "Can't grab frame! Stop\n";
-            break;
-        }
-
-        resize(frame, frame, Size(frameWidth, frameHeight));
-
-        // Inference
-        Mat faces;
-        tm.start();
-        detector->detect(frame, faces);
-        tm.stop();
-
-        Mat result = frame.clone();
-        // Draw results on the input image
-        visualize(result, nFrame, faces, tm.getFPS());
-
-        // Visualize results
-        imshow("Live", result);
-
-        int key = waitKey(1);
-        bool saveFrame = save;
-        if (key == ' ')
-        {
-            saveFrame = true;
-            key = 0;  // handled
-        }
-
-        if (saveFrame)
-        {
-            std::string frame_name = cv::format("frame_%05d.png", nFrame);
-            std::string result_name = cv::format("result_%05d.jpg", nFrame);
-            cout << "Saving '" << frame_name << "' and '" << result_name << "' ...\n";
-            imwrite(frame_name, frame);
-            imwrite(result_name, result);
-        }
-        writer.write(result);
-
-        ++nFrame;
-
-        if (key > 0 || nFrame > 400)
-            break;
-    }
+  if (outputVideo.length() > 0)
     writer.release();
-    cout << "Processed " << nFrame << " frames" << endl;
-    
-    cout << "Done." << endl;
-    return 0;
+  cout << "Processed " << nFrame << " frames" << endl;
+
+  cout << "Done." << endl;
+  return 0;
 }
