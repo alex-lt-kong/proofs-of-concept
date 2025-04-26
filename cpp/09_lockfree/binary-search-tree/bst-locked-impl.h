e587f072e2cea9b87c5b1ad990cc430ffdbf4548
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-04-26 14:50:18 +0800

    closer to a bug-free impl

--- cpp/09_lockfree/binary-search-tree/bst-locked-impl.h
+++ cpp/09_lockfree/binary-search-tree/bst-locked-impl.h
@@ -2,6 +2,7 @@
 #define BST_LOCKED_IMPL_H
 
 #include <print>
+#include <vector>
 
 namespace PoC::LockFree {
     struct TreeNode {
@@ -17,7 +18,13 @@ namespace PoC::LockFree {
         static int find_successor(TreeNode *root, TreeNode *node) {}
 
     public:
-        static void insert(TreeNode *root, int val) {
+        // insert() method passed all tests at
+        // https://leetcode.com/problems/insert-into-a-binary-search-tree/submissions/1618140923/
+        static TreeNode *insert(TreeNode *root, int val) {
+            if (root == nullptr) {
+                root = new TreeNode(val);
+                return root;
+            }
             if (root->val > val) {
                 if (root->left == nullptr) {
                     root->left = new TreeNode(val);
@@ -33,20 +40,50 @@ namespace PoC::LockFree {
             } else {
                 std::println("Node exists");
             }
+            return root;
         }
 
-        static void inorder_traversal(TreeNode *root) {
+        // inorder_traversal() method passed all tests at
+        // https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/1618146714/
+        // Note that this problem is inorder traversal only, not limited to BST
+        static std::vector<int> inorder_traversal(TreeNode *root) {
+            if (root == nullptr) {
+                return {};
+            }
+            std::vector<int> result;
             if (root->left != nullptr) {
-                inorder_traversal(root->left);
+                auto left_result = inorder_traversal(root->left);
+                result.insert(result.end(), left_result.begin(),
+                              left_result.end());
             }
-            std::print("{}, ", root->val);
+            result.push_back(root->val);
+            std::print("{:>6}, ", root->val);
             if (root->right != nullptr) {
-                inorder_traversal(root->right);
+                auto right_result = inorder_traversal(root->right);
+                result.insert(result.end(), right_result.begin(),
+                              right_result.end());
             }
+            return result;
         }
+
+        // search() method passed all tests at
+        // https://leetcode.com/problems/search-in-a-binary-search-tree/submissions/1618143004/
+        static TreeNode *search(TreeNode *root, int val) {
+            if (root == nullptr) {
+                return nullptr;
+            }
+            if (root->val == val) {
+                return root;
+            }
+            if (root->val > val) {
+                return search(root->left, val);
+            }
+            return search(root->right, val);
+        }
+
 
         static void delete_node(TreeNode **root, int data) {
-            if (root == nullptr) {
+            if (*root == nullptr) {
                 return;
             }
             if (data < (*root)->val) {
@@ -54,29 +91,56 @@ namespace PoC::LockFree {
             } else if (data > (*root)->val) {
                 delete_node(&((*root)->right), data);
             } else if ((*root)->val == data) {
-                // Node with two children: Get the inorder successor (smallest
-                // in the right subtree)
-                if ((*root)->right != nullptr) {
-                    TreeNode **successorNode = &((*root)->right);
-                    while ((*successorNode)->left) {
-                        //  successorParent = successorNode;
-                        successorNode = &((*successorNode)->left);
+                if ((*root)->left == nullptr && (*root)->right == nullptr) {
+                    delete *root;
+                    *root = nullptr;
+                } else if ((*root)->left == nullptr ||
+                           (*root)->right == nullptr) {
+                    std::print("Deleteing {} with one child, ", (*root)->val);
+                    // No need to look further, just promote the only child as
+                    // parent
+                    if ((*root)->left != nullptr) {
+                        std::println("left child");
+                        TreeNode *new_root = (*root)->left;
+                        delete *root;
+                        *root = new_root;
+                    } else {
+                        std::println("right child");
+                        TreeNode *new_root = (*root)->right;
+                        delete *root;
+                        *root = new_root;
                     }
-                    (*root)->val = (*successorNode)->val;
-                    *successorNode = nullptr;
-                } else if ((*root)->left != nullptr) {
-                    std::println("if (root->left != nullptr)");
-                    TreeNode **successorNode = &((*root)->left);
-                    while ((*successorNode)->right) {
-                        successorNode = &((*successorNode)->right);
+                } else {
+                    std::println("Deleteing {} with both children",
+                                 (*root)->val);
+                    TreeNode **new_root = &((*root)->right);
+                    while ((*new_root)->left != nullptr) {
+                        new_root = &((*new_root)->left);
                     }
-                    (*root)->val = (*successorNode)->val;
-                    *successorNode = nullptr;
+                    (*new_root)->left = (*root)->left;
+                    // delete *root;
+                    //(*root)->val = (*new_root)->val;
+                    *root = *new_root;
+                    // *root
                 }
-
             } else {
                 std::println("not found");
             }
+        }
+
+        static void visualize_tree(TreeNode *root, int depth = 0,
+                                   char branch = ' ') {
+            if (root == nullptr) {
+                return;
+            }
+            // Print right subtree
+            visualize_tree(root->right, depth + 1, '/');
+
+            // Print current node value with appropriate indentation
+            std::print("{:>{}}{}{}\n", "", 6 * depth, branch, root->val);
+
+            // Print left subtree
+            visualize_tree(root->left, depth + 1, '\\');
         }
     };
 } // namespace PoC::LockFree
--- cpp/09_lockfree/binary-search-tree/main.cpp
+++ cpp/09_lockfree/binary-search-tree/main.cpp
@@ -11,6 +11,10 @@ int main() {
     LockedBinarySearchTree::insert(root, 1314);
     LockedBinarySearchTree::insert(root, 65536);
     LockedBinarySearchTree::insert(root, 3);
+    LockedBinarySearchTree::insert(root, 4);
+    LockedBinarySearchTree::insert(root, 5);
+    LockedBinarySearchTree::insert(root, -213);
+    LockedBinarySearchTree::insert(root, -3);
     LockedBinarySearchTree::insert(root, -23);
     LockedBinarySearchTree::inorder_traversal(root);
     std::println();
@@ -19,19 +23,28 @@ int main() {
     LockedBinarySearchTree::inorder_traversal(root);
     std::println();
 
-    /*
-    LockedBinarySearchTree::delete_node(root, -10086);
-    LockedBinarySearchTree::inorder_traversal(root);
-    std::println();
-    */
     LockedBinarySearchTree::delete_node(&root, 3);
     LockedBinarySearchTree::inorder_traversal(root);
     std::println();
+
     LockedBinarySearchTree::delete_node(&root, -23);
     LockedBinarySearchTree::inorder_traversal(root);
     std::println();
+
+    LockedBinarySearchTree::insert(root, 51);
+    LockedBinarySearchTree::insert(root, -23);
+    LockedBinarySearchTree::insert(root, -32345);
+    LockedBinarySearchTree::insert(root, -32346);
+    LockedBinarySearchTree::insert(root, -32344);
+    LockedBinarySearchTree::insert(root, -76);
+    LockedBinarySearchTree::inorder_traversal(root);
+    std::println();
+    LockedBinarySearchTree::visualize_tree(root);
+    std::println();
     LockedBinarySearchTree::delete_node(&root, -10086);
     LockedBinarySearchTree::inorder_traversal(root);
+    std::println();
+    LockedBinarySearchTree::visualize_tree(root);
     std::println();
     return 0;
 }
