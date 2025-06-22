41a42e2528ec37f0f22f7056cb4405acca453a19
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-06-22 20:53:44 +0800

    TWS API can get market data now

--- cpp/13_tws-api/src/TestCppClient/Main.cpp
+++ cpp/13_tws-api/src/TestCppClient/Main.cpp
@@ -7,10 +7,9 @@
 #include "StdAfx.h"
 #include "client/Contract.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <chrono>
+#include <cstdio>
+#include <spdlog/spdlog.h>
 #include <thread>
 
 #include "TestCppClient.h"
@@ -18,70 +17,78 @@
 const unsigned MAX_ATTEMPTS = 50;
 const unsigned SLEEP_TIME = 10;
 
-/* IMPORTANT: always use your paper trading account. The code below will submit orders as part of the demonstration. */
+/* IMPORTANT: always use your paper trading account. The code below will submit
+ * orders as part of the demonstration. */
 /* IB will not be responsible for accidental executions on your live account. */
-/* Any stock or option symbols displayed are for illustrative purposes only and are not intended to portray a recommendation. */
-/* Before contacting our API support team please refer to the available documentation. */
+/* Any stock or option symbols displayed are for illustrative purposes only and
+ * are not intended to portray a recommendation. */
+/* Before contacting our API support team please refer to the available
+ * documentation. */
 
-int main(int argc, char** argv)
-{
-	const char* host = argc > 1 ? argv[1] : "";
-	int port = argc > 2 ? atoi(argv[2]) : 0;
-	if (port <= 0)
-		port = 7496;
-	const char* connectOptions = argc > 3 ? argv[3] : "+PACEAPI";
-	int clientId = 0;
+int main(int argc, char **argv) {
+    const char *host = argc > 1 ? argv[1] : "";
+    int port = argc > 2 ? atoi(argv[2]) : 0;
+    if (port <= 0)
+        port = 7496;
+    const char *connectOptions = argc > 3 ? argv[3] : "+PACEAPI";
+    int clientId = 0;
 
-	unsigned attempt = 0;
-	printf( "Start of C++ Socket Client Test %u\n", attempt);
+    unsigned attempt = 0;
+    printf("Start of C++ Socket Client Test %u\n", attempt);
 
-	for (;;) {
-		++attempt;
-		printf( "Attempt %u of %u\n", attempt, MAX_ATTEMPTS);
+    for (;;) {
+        ++attempt;
+        printf("Attempt %u of %u\n", attempt, MAX_ATTEMPTS);
 
-		TestCppClient client;
+        TestCppClient client;
 
-		// Run time error will occur (here) if TestCppClient.exe is compiled in debug mode but TwsSocketClient.dll is compiled in Release mode
-		// TwsSocketClient.dll (in Release Mode) is copied by API installer into SysWOW64 folder within Windows directory
+        // Run time error will occur (here) if TestCppClient.exe is compiled in
+        // debug mode but TwsSocketClient.dll is compiled in Release mode
+        // TwsSocketClient.dll (in Release Mode) is copied by API installer into
+        // SysWOW64 folder within Windows directory
 
-		if( connectOptions) {
-			client.setConnectOptions( connectOptions);
-		}
+        if (connectOptions) {
+            client.setConnectOptions(connectOptions);
+        }
 
-		client.connect( host, port, clientId);
-		// Define a contract - Example for Tesla stock
-		Contract contract;
-		contract.symbol = "TSLA";
-		contract.secType = "STK";
-		contract.currency = "USD";
-		contract.exchange = "SMART";
+        client.connect(host, port, clientId);
+        // Define a contract - Example for Tesla stock
+        Contract contract;
+        contract.symbol = "TSLA";
+        contract.secType = "STK";
+        contract.currency = "USD";
+        contract.exchange = "SMART";
 
-	  /*
-		contract.symbol = "EUR";
-		contract.secType = "CASH";  // For Forex
-		contract.currency = "USD";
-		contract.exchange = "IDEALPRO";*/
+        /*
+          contract.symbol = "EUR";
+          contract.secType = "CASH";  // For Forex
+          contract.currency = "USD";
+          contract.exchange = "IDEALPRO";*/
+        contract.symbol = "BTC";
+        contract.secType = "CRYPTO";
+        contract.exchange = "OSL";
+        contract.currency = "USD";
+        // Set market data type to DELAYED (3)
+        client.m_pClient->reqMarketDataType(3);
+        // Request market data
+        // Parameters: tickerId (unique identifier), contract, genericTickList,
+        // snapshot, regulatorySnapshot
+        // client.m_pClient->reqMktData(1, contract, "", false, false,
+        // TagValueListSPtr());
+        client.m_pClient->reqTickByTickData(1, contract, "BidAsk", 10, false);
+        // client.m_pClient->allowRedirect()
+        while (client.isConnected()) {
+            // client.tickDataOperation();
+            client.processMessages();
+            spdlog::info("client.processMessages()");
+        }
+        if (attempt >= MAX_ATTEMPTS) {
+            break;
+        }
 
-		// Set market data type to DELAYED (3)
-		client.m_pClient->reqMarketDataType(3);
-		// Request market data
-		// Parameters: tickerId (unique identifier), contract, genericTickList, snapshot, regulatorySnapshot
-		client.m_pClient->reqMktData(1, ContractSamples::EurGbpFx(), "", false, false, TagValueListSPtr());
-	        //client.m_pClient->reqTickByTickData(1, contract, "Last", 10, false);
-		//client.m_pClient->allowRedirect()
-		while( client.isConnected()) {
-			//client.tickDataOperation();
-			client.processMessages();
-			std::cout << "client.processMessages()\n";
-		}
-		if( attempt >= MAX_ATTEMPTS) {
-			break;
-		}
+        printf("Sleeping %u seconds before next attempt\n", SLEEP_TIME);
+        std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
+    }
 
-		printf( "Sleeping %u seconds before next attempt\n", SLEEP_TIME);
-		std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
-	}
-
-	printf ( "End of C++ Socket Client Test\n");
+    printf("End of C++ Socket Client Test\n");
 }
-
